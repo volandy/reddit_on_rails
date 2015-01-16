661e10eb95bff4152a43a50e9f56cbebc587b993
volandy <volandy14@gmail.com>
2015-01-17 00:00:06 +0200

    new editing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 RedditOnRails::Application.routes.draw do
+
+  resources :pages
+
+  root :to => "pages#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
